cac81dd8fc091e4d78f117cad4be891333baddc1
Dale Seo <dale.seo@gmail.com>
2016-09-30 14:22:12 +0900

    hellow browserify

--- browserify/main.js
+++ browserify/main.js
@@ -1,3 +1,9 @@
+/* custom module */
 var exp = require('./exp');
 console.log(exp.str);
 console.log(exp.func(3));
+
+/* external module */
+var unique = require('uniq');
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+console.log(unique(data));
