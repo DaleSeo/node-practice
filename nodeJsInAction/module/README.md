1afa7a9cfb8a956b725e7b1efa1f38c5bee13145
Dale Seo <dale.seo@gmail.com>
2017-01-01 19:26:51 +0900

    nodeJsInAction module

---
+++ nodeJsInAction/module/README.md
@@ -0,0 +1,13 @@
+# Module
+
+# Run
+```
+$ node test-currency-functions.js
+```
+```
+$ node test-currency-class.js
+```
+
+# References
+ - https://github.com/wikibook/nodejs-in-action/blob/master/3/currency.js
+ - https://github.com/wikibook/nodejs-in-action/blob/master/3/test-currency.js
---
+++ nodeJsInAction/module/test-currency-class.js
@@ -0,0 +1,9 @@
+var Currency = require('./currency-class');
+
+var currency = new Currency(0.91);
+
+console.log('50 Canadian dollars equals this amount of US dollars:');
+console.log(currency.canadianToUs(50));
+
+console.log('30 US dollars equals this amount of Canadian dollars:');
+console.log(currency.usToCanadian(30));
