3a6d70cbbf404d5a8ffbd89b9f8b871d47265cd3
Dale Seo <dale.seo@gmail.com>
2017-05-21 10:51:36 +0900

    flash

---
+++ flash/README.md
@@ -0,0 +1,2 @@
+https://www.npmjs.com/package/connect-flash
+http://stackoverflow.com/questions/23160743/how-to-send-flash-messages-in-express-4-0
---
+++ flash/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+
+const app = express()
+
+app.use(expressSession({
+  secret: 'my key',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(flash())
+
+app.get('/flash', (req, res) => {
+  // Set a flash message by passing the key, followed by the value, to req.flash().
+  req.flash('info', 'Flash is back!')
+  res.redirect('/')
+})
+
+app.get('/', (req, res) => {
+  // Get an array of flash messages by passing the key to req.flash()
+  res.send(req.flash('info'))
+})
+
+app.listen(3000, console.log.bind(console, 'Server is running.'))
